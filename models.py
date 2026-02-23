a7db804d6011728571c7e226bb05069d7832a85e
ashish <ashishsinghgermany@gmail.com>
2026-02-23 14:50:04 +0100

    Define User and Movie SQLAlchemy models

--- models.py
+++ models.py
@@ -1,3 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    movies = db.relationship('Movie', backref='user', cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'<User {self.name}>'
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    director = db.Column(db.String(100))
+    year = db.Column(db.Integer)
+    poster_url = db.Column(db.String(300))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Movie {self.name}>'
